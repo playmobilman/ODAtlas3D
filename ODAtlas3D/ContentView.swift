1d4923d1bde31763c23d9eb56d20de2e95ab4c56
Matías Schmid <mschmid@intermedia.com.uy>
2022-03-26 11:26:09 -0300

    Initial Commit

---
+++ ODAtlas3D/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ODAtlas3D
+//
+//  Created by Matias Schmid on 26/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ODAtlas3D/ODAtlas3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  ODAtlas3DApp.swift
+//  ODAtlas3D
+//
+//  Created by Matias Schmid on 26/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ODAtlas3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
